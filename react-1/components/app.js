1523c9e17f50b542a7b3f0d9df6d7f823eb092cc
Ripmma <Ripmma@gmail.com>
2018-01-11 21:27:14 -0500

    FINALLY DONE!

---
+++ react-1/components/app.js
@@ -0,0 +1,33 @@
+var React = require('react');
+var Popular = require('./popular');
+var ReactRouter = require('react-router-dom');
+var Router = ReactRouter.BrowserRouter;
+var Route = ReactRouter.Route;
+var Switch = ReactRouter.Switch;
+var Nav = require('./nav');
+var Home = require('./home');
+var Battle = require('./battle');
+var Results = require('./results');
+
+class App extends React.Component {
+  render() {
+    return (
+      <Router>
+	      <div className='container'>
+	      	<Nav />
+	      	<Switch>
+		      	<Route exact path='/' component={Home} />
+		      	<Route exact path='/battle' component={Battle} />
+		      	<Route path='/battle/results' component={Results} />
+		      	<Route path='/popular' component={Popular} />
+		      	<Route render={function(){
+		      		return <p>Not Found</p>
+		      	}} />
+		      </Switch>
+	      </div>
+      </Router>
+    )
+  }
+}
+
+module.exports = App;
